6e2d88b56c571975878ee4cfbd8be824cf5c95cf
kentora <kentora@kentora.dk>
2013-09-30 12:18:34 +0200

    Plugin functionality complete (give or take)
    But it could use some formatting...!

--- ExcelToWP.php
+++ ExcelToWP.php
@@ -22,9 +22,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-?>
 
-<?php
 	require_once('excel_reader2.php');
 	
 	/** Register admin menu hook */
@@ -41,17 +39,60 @@
 			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 		}
 		if($_POST['stage'] == 1){
+			$err = false;
 			if(!$_FILES['excelUpload']['error'] > 0){
-				echo $_FILES['excelUpload']['name'] . ' uploaded as ' . $_FILES['excelUpload']['tmp_name'];
-				echo '<br />';
-				echo 'Type is: ' . $_FILES['excelUpload']['type'] . ' and extension is ' . end(explode('.', $_FILES['excelUpload' ]['name']));
-				echo '<br />';
-				echo 'dump:<br />';
 				error_reporting(E_ALL ^ E_NOTICE);
-				wp_upload_bits("ExcelToWP.xls", null, file_get_contents($_FILES['excelUpload']['tmp_name']));
-				$data = new Spreadsheet_Excel_Reader(trailingslashit(wp_upload_dir()['path']) . 'ExcelToWP.xls');
-				echo $data->dump(true, true);
+				$wp_upload = wp_upload_bits("ExcelToWP.xls", null, file_get_contents($_FILES['excelUpload']['tmp_name']));
+				if(!$wp_upload['error']){
+					$data = new Spreadsheet_Excel_Reader($wp_upload['file'], false);
+					
+					$row = 2;
+						
+						$names = explode(' ', $data->val($row, 1));
+						$namesCount = count($names);
+						
+						$first_name = getFirstName($names, $namesCount -1);
+						
+						if($namesCount > 1){
+							$last_name = $names[$namesCount -1];
+						} else {
+							$last_name = '';
+						}
+					
+					while($data->val($row, 2) != ""){
+						$userdat = array(
+							'user_pass' => $data->val($row, 4),
+							'user_login' => $data->val($row, 2),
+							'user_email' => $data->val($row, 3),
+							'display_name' => $data->val($row, 1),
+							'first_name' => $first_name,
+							'last_name' => $last_name,
+							'role' => 'author'
+						);
+						
+						echo 'Trying to add user ' . $userdat['user_login'] . '... ';
+						
+						$insertResult = wp_insert_user($userdat);
+						
+						if(is_wp_error($insertResult)){
+							echo '<b style="color: red"> Error: ' . $insertResult->get_error_message($insertResult->get_error_code()) . '</b>';
+						} else {
+							echo '<b style="color: green">Succes</b>';
+						}
+						
+						echo '<br />';
+						$row++;
+					}
+					
+					unlink($wp_upload['file']);
+				} else {
+					$err = true;
+				}
 			} else {
+				$err = true;
+			}
+			
+			if($err){
 				echo 'Error on upload...<br />' . $_FILES['excelUpload']['error'];
 			}
 		} else if($_POST['stage'] == 2){
@@ -63,5 +104,15 @@
 			echo '<input type="submit" value="import" />';
 			echo '</form>';
 		}
+	}
+	
+	function getFirstName($arr, $count){
+		$name = '';
+		
+		for($i = 0; $i < $count; $i++){
+			$name .= $arr[$i];
+		}
+		
+		return $name;
 	}
 ?>
